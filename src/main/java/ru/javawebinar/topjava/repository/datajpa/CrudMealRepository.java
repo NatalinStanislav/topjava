0204c80eb4a7056016ca892cae0304fc5734d134
NatalinStanislav <jylbars2307@gmail.com>
2020-09-14 01:13:43 +0300

    HW05 - Optional, subtask 7.2

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -22,4 +22,8 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
 
     @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC")
     List<Meal> getBetweenHalfOpen(@Param("startDateTime") LocalDateTime startDateTime, @Param("endDateTime") LocalDateTime endDateTime, @Param("userId") int userId);
+
+    @Transactional
+    @Query("SELECT m FROM Meal m LEFT JOIN FETCH m.user WHERE m.id = :id AND m.user.id = :userId")
+    Meal getMealWithUser(@Param("id") int id, @Param("userId") int userId);
 }
--- src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -1,9 +1,20 @@
 package ru.javawebinar.topjava.service.datajpa;
 
+import org.junit.Test;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.Profiles;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealServiceTest;
+
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(profiles = Profiles.DATAJPA)
 public class DataJpaMealServiceTest extends MealServiceTest {
+    @Test
+    public void getWithUser() throws Exception {
+        Meal meal = service.getWithUser(MEAL1.id(), USER_ID);
+        MEAL_MATCHER.assertMatch(meal, MEAL1);
+        USER_MATCHER.assertMatch(meal.getUser(), USER);
+    }
 }
